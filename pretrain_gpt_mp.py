da656df553c5b829fb7068334d73a02487a9bf87
yier <56420072+liyier90@users.noreply.github.com>
2023-06-08 09:51:54 +0800

    feat: modify gpt pretrain scripts to be XLA friendly

--- pretrain_gpt_mp.py
+++ pretrain_gpt_mp.py
@@ -1,26 +1,40 @@
-# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+# coding=utf-8
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """Pretrain GPT"""
 
-import os
-
 import torch
-import torch_xla.core.xla_model as xm
 from functools import partial
 from megatron import get_args
 from megatron import print_rank_0
 from megatron import get_timers
 from megatron import get_tokenizer
-from megatron.core import tensor_parallel
-from megatron.core import parallel_state
-from megatron.core.enums import ModelType
+from megatron import mpu
 from megatron.data.gpt_dataset import build_train_valid_test_datasets
-from megatron.model import GPTModel
+from megatron.model import GPTModel, ModelType
 from megatron.training import pretrain
 from megatron.utils import get_ltor_masks_and_position_ids
 from megatron.utils import average_losses_across_data_parallel_group
+import torch_xla.core.xla_model as xm
+import torch_xla.distributed.xla_multiprocessing as xmp
+import os
+import torch_xla.debug.metrics as met
 
-os.environ['NEURON_CC_FLAGS'] = '--model-type transformer'
+import torch_xla.debug.profiler as xp
+
+os.environ["NEURON_CC_FLAGS"] = "--model-type transformer"
 
 def model_provider(pre_process=True, post_process=True):
     """Build the model."""
@@ -50,11 +64,11 @@ def get_batch(data_iterator):
         data = next(data_iterator)
     else:
         data = None
-    # data_b = tensor_parallel.broadcast_data(keys, data, datatype)
+    #data_b = mpu.broadcast_data(keys, data, datatype)
     data_b = data
 
     # Unpack.
-    # tokens_ = data_b['text'].long()
+    #tokens_ = data_b['text'].long()
     tokens_ = data_b['text'].int()
     labels = tokens_[:, 1:].contiguous()
     tokens = tokens_[:, :-1].contiguous()
@@ -73,14 +87,13 @@ def loss_func(loss_mask, output_tensor):
     losses = output_tensor.float()
     loss_mask = loss_mask.view(-1).float()
     loss = torch.sum(losses.view(-1) * loss_mask) / loss_mask.sum()
-
-    if parallel_state.get_data_parallel_world_size():
+    
+    if mpu.get_data_parallel_world_size() > 1:
         # Reduce loss for logging.
         averaged_loss = average_losses_across_data_parallel_group([loss])
-
         return loss, {'lm loss': averaged_loss[0]}
-
-    return loss, {'lm loss': loss}
+    else:
+        return loss, {'lm loss': loss}
 
 
 def forward_step(data_iterator, model):
@@ -89,11 +102,10 @@ def forward_step(data_iterator, model):
     timers = get_timers()
 
     # Get the batch.
-    timers('batch-generator', log_level=2).start()
+    timers('batch-generator').start()
     tokens, labels, loss_mask, attention_mask, position_ids = get_batch(
         data_iterator)
     timers('batch-generator').stop()
-
     output_tensor = model(tokens, position_ids, attention_mask,
                           labels=labels)
 
@@ -113,20 +125,23 @@ def train_valid_test_datasets_provider(train_val_test_num_samples):
         train_valid_test_num_samples=train_val_test_num_samples,
         seq_length=args.seq_length,
         seed=args.seed,
-        skip_warmup=(not args.mmap_warmup),
-        train_data_prefix=args.train_data_path,
-        valid_data_prefix=args.valid_data_path,
-        test_data_prefix=args.test_data_path,
-        data_cache_path=args.data_cache_path)
+        skip_warmup=(not args.mmap_warmup))
     print_rank_0("> finished creating GPT datasets ...")
-
     return train_ds, valid_ds, test_ds
 
 
-if __name__ == '__main__':
-
-    pretrain(train_valid_test_datasets_provider,
-             model_provider,
+def pretrain_mp(rank, world_size):
+    os.environ['RANK'] = str(rank)
+    os.environ['WORLD_SIZE'] = str(world_size)
+    pretrain(train_valid_test_datasets_provider, model_provider,
              ModelType.encoder_or_decoder,
-             forward_step,
-             args_defaults={'tokenizer_type': 'GPT2BPETokenizer'})
+             forward_step, args_defaults={'tokenizer_type': 'GPT2BPETokenizer'})
+    xm.rendezvous('ending')
+    #xm.mark_step()
+
+if __name__ == '__main__':
+    world_size = int(os.environ['NEURON_NUM_DEVICES'])
+    xmp.spawn(pretrain_mp,
+        args=(world_size,),
+        nprocs=world_size,
+        join=True)
